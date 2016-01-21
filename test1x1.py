c2a8da78b5bb029b967eec4609cdd8ba01418460
phstearns <phstearns13@gmail.com>
2016-01-21 14:46:36 -0500

    Updated from Brython Server: 1/21/2016 2:46:35 PM

--- test1x1.py
+++ test1x1.py
@@ -2,4 +2,13 @@ print("Welcome")
 print("to test program 1x1")
 print("Hello, World!")
 c= input("input number: ")
+c=float(c)
 print(c)
+a= 15
+b=37
+print(a+b)
+print(a+c)
+print(b+c)
+print(a-b)
+print(a-c)
+print(b-c)
